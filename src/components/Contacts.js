9da607b5a2351488bccc13e07b6f760494faf4ab
Carlos Plata <carlosplata@gmail.com>
2023-08-31 11:43:23 -0500

    added prop-types validation

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 /**
  * Contacts.js
  * 
@@ -19,4 +20,8 @@ const Contacts = ({ contacts, onDeleteContact }) => {
 	</ol>;
 }
 
+Contacts.propTypes = {
+	contacts: PropTypes.array.isRequired,
+	onDeleteContact: PropTypes.func.isRequired
+}
 export default Contacts;
