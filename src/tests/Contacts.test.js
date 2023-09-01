25aae4544d6538f44a5a154ec24fde581303ee95
Carlos Plata <carlosplata@gmail.com>
2023-08-31 22:59:42 -0500

    updated test cases

--- src/tests/Contacts.test.js
+++ src/tests/Contacts.test.js
@@ -31,6 +31,7 @@ const contacts = [
 ];
 
 describe('Contacts component', () => {
+	// Test: renders a list of contacts with correct details
 	it('renders a list of contacts with correct details', () => {
 		render(<Contacts contacts={contacts} />);
 
@@ -46,6 +47,7 @@ describe('Contacts component', () => {
 		});
 	});
 
+	// Test: calls onDeleteContact when "Remove" button is clicked
 	it('calls onDeleteContact when "Remove" button is clicked', () => {
 		const mockDeleteFunction = jest.fn();
 		render(<Contacts contacts={contacts} onDeleteContact={mockDeleteFunction} />);
@@ -57,6 +59,55 @@ describe('Contacts component', () => {
 			expect(mockDeleteFunction).toHaveBeenCalledWith(contact);
 		});
 	});
+
+	// Test: updates the search query on input change
+	it('updates the search query on input change', () => {
+		render(<Contacts contacts={contacts} onDeleteContact={jest.fn()} />);
+
+		const searchInput = screen.getByPlaceholderText('Search Contacts');
+		fireEvent.change(searchInput, { target: { value: 'Richard' } });
+
+		expect(searchInput.value).toBe('Richard');
+	});
+
+	// Test: clears the search query on "Show All" button click
+	it('clears the search query on "Show All" button click', () => {
+		render(<Contacts contacts={contacts} onDeleteContact={jest.fn()} />);
+
+		const searchInput = screen.getByPlaceholderText('Search Contacts');
+		fireEvent.change(searchInput, { target: { value: 'Karen' } });
+
+		const showAllButton = screen.getByText('Show All');
+		fireEvent.click(showAllButton);
+
+		expect(searchInput.value).toBe('');
+	});
+
+	// Test: displays correct count of shown contacts
+	it('displays correct count of shown contacts', () => {
+		render(<Contacts contacts={contacts} onDeleteContact={jest.fn()} />);
+
+		const searchInput = screen.getByPlaceholderText('Search Contacts');
+		fireEvent.change(searchInput, { target: { value: 'Richard' } });
+
+		const showingContactsMessage = screen.getByText('Now showing 1 of 3.');
+		expect(showingContactsMessage).toBeInTheDocument();
+	});
+
+	// Test: shows all contacts when query is cleared
+	it('shows all contacts when query is cleared', () => {
+		render(<Contacts contacts={contacts} onDeleteContact={jest.fn()} />);
+
+		const searchInput = screen.getByPlaceholderText('Search Contacts');
+		fireEvent.change(searchInput, { target: { value: 'Richard' } });
+
+		const showAllButton = screen.getByText('Show All');
+		fireEvent.click(showAllButton);
+
+		const nameElements = screen.getAllByText(/Karen|Richard|Tyler/);
+		expect(nameElements.length).toBe(3);
+	});
+
 });
 
 export { contacts };
